a1e54d36b7c8b7042749700f3dbe0d1e26c6e4ac
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-04-07 15:16:35 -0700

    finished the navigation bar component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Switch, Route } from "react-router-dom"; 
 
 import "../App.css"; 
 
+
+import Dashboard from './Dashboard';
+
 const App = () => {
     return (
-        <div>
-            Hello
-        </div>
+        <Switch>
+            <Route path="/dashboard">
+                <Dashboard />
+            </Route>
+        </Switch>
     )
 }
 
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import styled from "styled-components"; 
+
+
+import NavigationBar from './NavigationBar';
+
+const StyledDashboard = styled.div`
+
+
+& {
+    display: flex; 
+    height: 100vh; 
+    background: #F4F7F9; 
+}
+
+
+
+`
+
 const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+        <StyledDashboard>
+            <NavigationBar />
+        </StyledDashboard>
     )
 }
 
