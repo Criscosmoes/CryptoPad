e87ada750a80acab5c7a09999e53b219ca8313ed
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-04-08 07:23:35 -0700

    first pull request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,9 +4,8 @@ import axios from "axios";
 export const fetchCrypto = () => async dispatch => {
 
 
-    const response = await axios.get("https://cors-anywhere.herokuapp.com/https://api.coinranking.com/v2/coin/Qwsogvtv82FCd/history?timePeriod=7d"); 
-
-    console.log(response); 
-
+    
+   
 
 }
+
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -84,7 +84,6 @@ const Dashboard = ({fetchCrypto}) => {
 
         fetchCrypto(); 
 
-
     }, [])
 
 
